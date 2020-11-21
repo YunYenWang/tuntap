0874be82807c6dd325ba4ae5660d1f187f82b59a
Jack <jack@maxlong.com.tw>
2020-11-21 08:13:11 +0800

    TunTap has InputStream & OutputStream

--- src/main/java/org/idea/net/TunTap.java
+++ src/main/java/org/idea/net/TunTap.java
@@ -1,17 +1,124 @@
 package org.idea.net;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
-public class TunTap {
-
-	static native int open(String dev);
+public class TunTap implements Closeable {
 	
-	static native void close(int fd);
-	
-	static native int write(int fd, ByteBuffer d, int len);
-	
-	static native boolean await(int fd, long timeout);
-	
+	static native int open(String dev);	
+	static native void close(int fd);	
+	static native int write(int fd, ByteBuffer d, int len);	
+	static native boolean await(int fd, long timeout);	
 	static native int read(int fd, ByteBuffer d);
+	
+	final int fd;
+	
+	long timeout = 1000L;
+	
+	public TunTap(String dev) throws IOException {
+		System.loadLibrary("tuntap");
+		
+		fd = open(dev);
+		if (fd < 0) {
+			throw new IOException("Failed to open device: " + fd);
+		}
+	}
+	
+	public void setTimeout(long timeout) {
+		this.timeout = timeout;
+	}
+	
+	@Override
+	public void close() throws IOException {
+		close(fd);		
+	}
+	
+	public OutputStream getOutputStream() {
+		return new OutputStream() {
+			
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				ByteBuffer bb = ByteBuffer.allocateDirect(len);
+				bb.put(b, off, len);
+				bb.flip();
+				
+				TunTap.write(fd, bb, len);
+			}
+			
+			@Override
+			public void write(int b) throws IOException {
+				ByteBuffer bb = ByteBuffer.allocateDirect(1);
+				bb.put((byte) b);
+				bb.flip();
+				
+				TunTap.write(fd, bb, 1);												
+			}
+			
+			@Override
+			public void close() throws IOException {
+				TunTap.this.close();
+			}
+		};
+	}
+	
+	public InputStream getInputStream() {
+		return new InputStream() {
+			
+			@Override
+			public int read(byte[] b, int off, int len) throws IOException {
+				if (TunTap.await(fd, timeout) == false) {
+					return 0;
+				}
+				
+				ByteBuffer bb = ByteBuffer.allocateDirect(len);
+				int s = TunTap.read(fd, bb);
+				if (s < 0) {
+					throw new IOException("Failed to read: " + s);
+				}
+				
+				bb.limit(s);
+				
+				for (int i = 0;i < s;i++) {
+					b[off + i] = bb.get(i);
+				}
+				
+				return s;
+			}
 
+			@Override
+			public int read() throws IOException {
+				if (TunTap.await(fd, timeout) == false) {
+					return 0;
+				}
+				
+				ByteBuffer bb = ByteBuffer.allocateDirect(1);
+				int s = TunTap.read(fd, bb);
+				if (s < 0) {
+					throw new IOException("Failed to read: " + s);
+				}
+				
+				bb.limit(s);
+				
+				return bb.get(0);
+			}
+			
+			@Override
+			public int available() throws IOException {
+				return TunTap.await(fd, timeout)? 1 : 0; // XXX - could be better
+			}
+			
+			@Override
+			public long skip(long n) throws IOException {
+				throw new IOException("Not yet implemented");
+			}
+			
+			@Override
+			public void close() throws IOException {
+				TunTap.this.close();
+			}
+		};
+	}
 }
--- src/test/java/org/idea/net/TunTapTest.java
+++ src/test/java/org/idea/net/TunTapTest.java
@@ -1,7 +1,13 @@
 package org.idea.net;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.StringTokenizer;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TunTapTest {
 
+	@BeforeEach
+	void init() {
+		System.loadLibrary("tuntap");
+	}
+	
 	@Test
 	void test() {
-		System.loadLibrary("tuntap");
-		
 		int fd = TunTap.open("tap0");
 		log.info("fd: {}", fd);
 		
@@ -26,9 +35,81 @@ public class TunTapTest {
 			if (r) {			
 				int s = TunTap.read(fd, bb);				
 				log.info("bytes: {}", s);
+				
+				bb.limit(s);
+				
+				for (int i = 0;i < s;i++) {
+					System.out.printf("%02X ", bb.get(i) & 0x0FF);
+				}
+				
+//				TunTap.write(fd, bb, bb.remaining());
 			}
+			
+			System.out.println();
 		}
 		
 //		TunTap.close(fd);
+	}
+
+	byte[] toBytes(String s) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		StringTokenizer st = new StringTokenizer(s);
+		while (st.hasMoreTokens()) {
+			baos.write(Integer.parseInt(st.nextToken(), 16));
+		}
+		
+		return baos.toByteArray();
+	}
+	
+	@Test
+	void testWrite() {
+		String p = "FF FF FF FF FF FF 02 0E 90 FA 15 5B 08 06 00 01 08 00 06 04 00 01 02 0E 90 FA 15 5B 0A 00 00 01 00 00 00 00 00 00 0A 00 00 02";
+		
+		int fd = TunTap.open("tap0");
+		
+		ByteBuffer bb = ByteBuffer.allocateDirect(1500);
+		bb.put(toBytes(p));
+		bb.flip();
+		
+		TunTap.write(fd, bb, bb.remaining());
+	}
+	
+	// ======
+	
+	@Test
+	void testInputStream() throws IOException {
+		long timeout = 10000L;
+		
+		try (TunTap tt = new TunTap("tap0")) {
+			tt.setTimeout(timeout);
+			
+			byte[] b = new byte[1500];
+			int s;
+			InputStream is = tt.getInputStream();
+			while ((s = is.read(b)) >= 0) {
+				for (int i = 0;i < s;i++) {
+					System.out.printf("%02X ", b[i] & 0x0FF);
+				}
+				
+				System.out.println();
+			}
+		}		
+	}
+	
+	@Test
+	void testOutputStream() throws IOException {
+		String p = "FF FF FF FF FF FF 02 0E 90 FA 15 5B 08 06 00 01 08 00 06 04 00 01 02 0E 90 FA 15 5B 0A 00 00 01 00 00 00 00 00 00 0A 00 00 02";
+		
+		long timeout = 10000L;
+		
+		try (TunTap tt = new TunTap("tap0")) {
+			tt.setTimeout(timeout);
+			
+			OutputStream os = tt.getOutputStream();
+			
+			byte[] b = toBytes(p);
+			os.write(b);
+		}
 	}
 }
